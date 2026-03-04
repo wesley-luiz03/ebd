3a658d884c72c24212ec039b86118d6c51d3a5f7
Wesley Luiz - LinuxZorin OS <wesleydev03@gmail.com>
2026-03-04 17:10:19 -0300

    Atualizando quadro

--- script.js
+++ script.js
@@ -3,7 +3,29 @@
 const celulas = document.querySelectorAll("tbody td");
 
 celulas.forEach((celula) => {
+
+  // Ignora coluna do nome
   if (celula.cellIndex === 0) return;
 
+  // Remove cursor
   celula.style.cursor = "default";
+
+  // Se a célula tiver estrelas
+  if (celula.innerHTML.includes("⭐")) {
+
+    const quantidade = (celula.innerHTML.match(/⭐/g) || []).length;
+
+    let estrelasHTML = "";
+
+    for (let i = 1; i <= quantidade; i++) {
+      estrelasHTML += "⭐";
+
+      if (i % 5 === 0) {
+        estrelasHTML += "<br>";
+      }
+    }
+
+    celula.innerHTML = estrelasHTML;
+  }
+
 });
